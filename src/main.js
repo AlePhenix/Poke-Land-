ecf41834c08a888c9713cdee930e45992f3d73d0
Piña <ale.casp88@gmail.com>
2023-01-16 08:48:48 -0600

    Added introduction-section on the main

--- src/main.js
+++ src/main.js
@@ -37,22 +37,16 @@ function GetPokemon(e){
         };
     };
     if (e == 1) {
-        pokeGenerator(1, 11);
+        pokeGenerator(1, 14);
     }
     if (e == 2) {
-        pokeGenerator(11, 21);
+        pokeGenerator(130, 142);
     }
     if (e == 3) {
-        pokeGenerator(21, 31);
+        pokeGenerator(21, 34);
     }
     if (e == 4) {
-        pokeGenerator(31, 41);
-    }
-    if (e == 5) {
-        pokeGenerator(41, 51);
-    }
-    if (e == 6) {
-        pokeGenerator(51, 61);
+        pokeGenerator(143, 155);
     }
 };
 
@@ -60,12 +54,8 @@ function crearPokemon(pokemon, number){
     const img = document.createElement('img');
     img.src = pokemon.sprites.front_default;
 
-    const h3 = document.createElement('h3');
-    h3.textContent = pokemon.name;
-
     const div = document.createElement('div');
     div.appendChild(img);
-    div.appendChild(h3);
 
     document.querySelector(`.play-ground${number}`).appendChild(div);
 };
@@ -74,6 +64,4 @@ GetPokemon(1);
 GetPokemon(2);
 GetPokemon(3);
 GetPokemon(4);
-GetPokemon(5);
-GetPokemon(6);
 
